23474d34dec2e998d5372136d727ae8b0d7b8920
ZechariaBender <zkbndr@gmail.com>
2018-12-25 01:57:30 +0200

    Added AlarmFactory

--- app/src/main/java/com/zevnzac/jewishalarmclock/AddAlarmActivity.java
+++ app/src/main/java/com/zevnzac/jewishalarmclock/AddAlarmActivity.java
@@ -1,13 +1,37 @@
 package com.zevnzac.jewishalarmclock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddAlarmActivity extends AppCompatActivity {
 
+    Button addAlarmButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+        addAlarmButton = findViewById(R.id.addAlarmButton);
+        addAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAlarm();
+            }
+        });
+    }
+
+    public void addAlarm() {
+        EditText et = (EditText)findViewById(R.id.editTime);
+        String time = et.getText().toString();
+        if (time.isEmpty())
+            time = "12:00";
+        int hour = Integer.parseInt(time.split(":")[0]);
+        int minute = Integer.parseInt(time.split(":")[1]);
+        AlarmFactory.getFactory().insertAlarm(hour,minute);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/zevnzac/jewishalarmclock/MainActivity.java
+++ app/src/main/java/com/zevnzac/jewishalarmclock/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zevnzac.jewishalarmclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -11,55 +12,63 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<AlarmCardView> mAlarmlist;
+    private ArrayList<AlarmCardView> mAlarmViewlist;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mlayoutManager;
 
     private FloatingActionButton addButton;
 
+    private AlarmFactory alarmFactory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        createExampleList();
+        alarmFactory = AlarmFactory.getFactory();
         buildRecyclerView();
-
         addButton = findViewById(R.id.floatingActionButton);
-
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                insertItem();
+                openAddAlarm();
             }
         });
+    }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        updateAlarmViewList();
+    }
+
+    public void updateAlarmViewList() {
+        mAlarmViewlist = alarmFactory.getAlarmViewList();
+        mAdapter = new AlarmAdapter(mAlarmViewlist);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
     }
 
     public void insertItem() {
-        mAlarmlist.add(new AlarmCardView(R.drawable.ic_alarm, "7:00", "AM"));
-        mAdapter.notifyItemInserted(mAlarmlist.size());
+        alarmFactory.insertAlarm(7,30);
+        updateAlarmViewList();
     }
 
     public void removeItem(int position) {
-        mAlarmlist.remove(position);
+        mAlarmViewlist.remove(position);
         mAdapter.notifyItemRemoved(position);
-    }
-
-    public void createExampleList() {
-        mAlarmlist = new ArrayList<>();
     }
 
     public void buildRecyclerView() {
         mRecyclerView = findViewById(R.id.alarmRecyclerView);
         mRecyclerView.setHasFixedSize(true);
         mlayoutManager = new LinearLayoutManager(this);
-        mAdapter = new AlarmAdapter(mAlarmlist);
-
+        updateAlarmViewList();
         mRecyclerView.setLayoutManager(mlayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-
     }
 
+    public void openAddAlarm(){
+        Intent intent = new Intent(this, AddAlarmActivity.class);
+        startActivity(intent);
+    }
 }
